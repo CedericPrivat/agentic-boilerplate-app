9f835e55cf3830423703a81fa70fd962ceefaee6
Cederic Privat <cederic.privat@gmail.com>
2026-01-04 11:50:55 +0100

    feat: set up providers (theme, query, toaster)

---
+++ src/providers/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import type { ReactNode } from "react";
+
+import { QueryProvider } from "./query-provider";
+import { ThemeProvider } from "./theme-provider";
+import { ToasterProvider } from "./toaster-provider";
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <QueryProvider>
+        {children}
+        <ToasterProvider />
+      </QueryProvider>
+    </ThemeProvider>
+  );
+}
