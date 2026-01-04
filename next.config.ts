2e62cced849106c5280d0d325aee223ab13fec43
Cederic Privat <cederic.privat@gmail.com>
2026-01-04 12:07:09 +0100

    fix: move reactCompiler and typedRoutes out of experimental, add typecheck to pre-commit

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,8 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  experimental: {
-    typedRoutes: true,
-    reactCompiler: true,
-  },
+  reactCompiler: true,
+  typedRoutes: true,
 };
 
 export default withNextIntl(nextConfig);
